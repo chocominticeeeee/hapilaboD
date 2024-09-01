e89af1fda9767fcadbf63208640390058c008b9e
chocominticeeeeeeeee <avaparasa58@gmail.com>
2024-09-02 04:19:37 +0900

    lineAt

--- src/components/client/FootLineAt.jsx
+++ src/components/client/FootLineAt.jsx
@@ -2,7 +2,16 @@
 import React from "react";
 
 function FootLineAt() {
-    return <div className="FootLineAt">FootLineAt</div>;
+    return (
+        <div id="FootLineAt">
+            <div className="onlyNow">
+                <p className="line1">今だけ限定　　　</p>
+                <p>　　　配信開始で3000円プレゼント!</p>
+            </div>
+            <a href="">LINE登録で応募する</a>
+            <p className="attension">※LINEから応募し、配信開始すると3000円プレゼントいたします。</p>
+        </div>
+    );
 }
 
 export default FootLineAt;
