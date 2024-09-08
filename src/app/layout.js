b3c3332ca843436df3ddd5077a92b0f59a49d708
chocominticeeeeeeeee <avaparasa58@gmail.com>
2024-09-08 11:41:50 +0900

    OGPMetaDataばせの変更

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 const FONT = Zen_Maru_Gothic({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
+	metadataBase: new URL('https://master--hapilabo-d.netlify.app/'),
 	title: 'ライバー事務所「ハピラボ」',
 	description: 'ライバー事務所「ハピラボ」',
 	openGraph: {
