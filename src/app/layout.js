52772853445a3136c6ee6a374785f30a26170613
chocominticeeeeeeeee <avaparasa58@gmail.com>
2024-09-08 11:53:53 +0900

    OGPMetaDataばせの変更

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,32 +3,34 @@ import "./globals.css";
 
 const FONT = Zen_Maru_Gothic({ subsets: ["latin"], weight: ["400"] });
 
-export const metadata = {
-	metadataBase: new URL('https://master--hapilabo-d.netlify.app/'),
-	title: 'ライバー事務所「ハピラボ」',
-	description: 'ライバー事務所「ハピラボ」',
-	openGraph: {
-		type: 'website',
-		url: 'https://master--hapilabo-d.netlify.app/',
-		title: 'ライバー事務所「ハピラボ」',
-		description: 'ライバー事務所「ハピラボ」',
-		images: [
-			{
-				url: '/OGP1.webp',
-				width: 1200,
-				height: 630,
-				alt: 'ライバー事務所「ハピラボ」',
-			},
-		],
-	},
-	twitter: {
-		card: 'summary_large_image',
-		title: 'ライバー事務所「ハピラボ」',
-		description: 'ライバー事務所「ハピラボ」',
-		images: ['/OGP1.webp'],
-	},
-};
+const metaTitle = "ライバー事務所「ハピラボ」  スキマ時間にライブ配信！ ";
+const metaDescription = "【ハピラボ】は、家事・育児を頑張るあなたを応援するライブ配信事務所です。";
 
+export const metadata = {
+    metadataBase: new URL("https://master--hapilabo-d.netlify.app/"),
+    title: metaTitle,
+    description: metaDescription,
+    openGraph: {
+        type: "website",
+        url: "https://master--hapilabo-d.netlify.app/",
+        title: metaTitle,
+        description: metaDescription,
+        images: [
+            {
+                url: "/OGP1.webp",
+                width: 1200,
+                height: 630,
+                alt: "ライバー事務所「ハピラボ」",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: metaTitle,
+        description: metaDescription,
+        images: ["/OGP1.webp"],
+    },
+};
 
 export default function RootLayout({ children }) {
     return (
